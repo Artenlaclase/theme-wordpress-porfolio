7dc024227144ee2b533295c1f2321c2f5b63f60f
Artenlaclase <artenlaclase@gmail.com>
2025-01-13 23:24:14 -0300

    pagian de inicio

---
+++ page-home.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Template Name: Página de Inicio
+*/
+get_header();
+?>
+
+<main>
+    <h1>Mi Portafolio</h1>
+    <p>¡Bienvenido a mi espacio de proyectos!</p>
+</main>
+
+<?php get_footer(); ?>
